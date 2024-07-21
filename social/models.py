c03974eb261b6223c70ddc06d2511c3c6b93eeab
Aviral Ale <69aviral@gmail.com>
2024-07-21 22:55:29 +0545

    "Added default value to Post.content field"

--- social/models.py
+++ social/models.py
@@ -6,7 +6,7 @@ import mimetypes
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(max_length=512)
+    content = models.TextField(max_length=512, default="")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
